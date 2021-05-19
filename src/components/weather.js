8de99631fbdb42bf46b05e99fb1f9b9188d4e58f
Patrick Coffey <patrickcoffey505@gmail.com>
2021-05-18 19:26:21 -0500

    add icon to weather component

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import './styles.css';
 import { Card, Button } from 'semantic-ui-react'
 import moment from 'moment';
 
-const refresh = () => {
-    window.location.reload();
+class Weather extends Component {
+
+    refresh = () => {
+      window.location.reload();
+    }
+
+    render() {
+      return (
+        <Card>
+            <Card.Content>
+                <Card.Header className="header">{this.props.weatherData.name}</Card.Header>
+                <img src={ `http://openweathermap.org/img/w/${this.props.weatherData.weather[0].icon}.png` } />
+                <Button className="button" inverted color='blue' circular icon='refresh' onClick={this.refresh} />
+                <p>Temperature: {this.props.weatherData.main.temp}&deg;F</p>
+                <p>Sunrise: {new Date(this.props.weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+                <p>Sunset: {new Date(this.props.weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+                <p>Day: {moment().format('dddd')}</p>
+                <p>Date: {moment().format('LL')}</p>
+                <p>Description: {this.props.weatherData.weather[0].description}</p>
+            </Card.Content>
+        </Card>
+      )
+    }
 }
 
-const CardExampleCard = ({weatherData}) => (
-    <Card>
-        <Card.Content>
-            <Card.Header className="header">City: {weatherData.name}</Card.Header>
-            <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
-            <p>Temperature: {weatherData.main.temp}&deg;F</p>
-            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-            <p>Day: {moment().format('dddd')}</p>
-            <p>Date: {moment().format('LL')}</p>
-            <p>Description: {weatherData.weather[0].description}</p>
-        </Card.Content>
-    </Card>
-)
-
-export default CardExampleCard;
+export default Weather;
